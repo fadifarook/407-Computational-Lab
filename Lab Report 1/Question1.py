1e3a61120188b43a47310844f4fad6a94e1662c6
fadifarook <fadifarook@gmail.com>
2024-09-27 17:56:56 -0400

    Intermediary

--- Lab Report 1/Question1.py
+++ Lab Report 1/Question1.py
@@ -5,11 +5,6 @@ import os
 
 print("Part B")
 
-# Gets the absolute path of cdata.txt
-currentDir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(currentDir, 'cdata.txt')
-
-
 def std_1(array):
     """Function corresponding to Standard Deviation Equation 1 (Two pass)"""
 
@@ -35,7 +30,7 @@ def std_2(array):
 
 
 # Load the data from the file
-speed = np.loadtxt(filename)
+speed = np.loadtxt('cdata.txt')
 
 true_value = np.std(speed, ddof=1)
 std1_value = std_1(speed)
@@ -113,141 +108,3 @@ std3_value_mean1 = std_3(normalSequence1)
 std3_value_mean2 = std_3(normalSequence2)
 print("Mean = 0 ; New Standard Deviation: ", std3_value_mean1, "\t Relative Error: ", (std3_value_mean1 - true_value1)/ true_value1)
 print("Mean = 1e7 ; New Standard Deviation: ", std3_value_mean2, "\t Relative Error: ", (std3_value_mean2 - true_value2)/ true_value2)
-
-
-
-
-
-
-print("""\n\nQuestion 2""")
-
-
-def p(u):
-    return (1-u)**8
-
-def q(u):
-    return 1 - 8*u + 28*u**2 - 56*u**3 + 70*u**4 - 56*u**5 + 28*u**6 - 8*u**7 + u**8
-
-def xterms(u):
-    return np.array([(1-u)**8, -1, 8*u, -28*u**2, 56*u**3 , -70*u**4 , 56*u**5, -28*u**6, 8*u**7, -u**8])
-
-
-N = 500
-u_array = np.linspace(0.98, 1.02, N)
-
-plt.plot(u_array ,p(u_array), color='#a8ddb5', label='p(u)')
-plt.plot(u_array ,q(u_array), color='#43a2ca', label='q(u)')  # noisier
-plt.xlabel('u value')
-plt.ylabel('Function Value')
-plt.legend()
-# plt.show()
-
-
-difference_array = p(u_array) - q(u_array)
-plt.plot(u_array, difference_array, color='green')
-plt.show()
-
-"""Plot histogram"""
-plt.hist(difference_array, 100)
-plt.show()
-
-C = 1e-16
-numTerms = 10
-terms = xterms(u=1)
-squaredTerms = np.mean(terms**2)
-print("Standard Deviation = ", np.std(difference_array))
-print("Calculated Standard Deviation = ", C * np.sqrt(numTerms) * np.sqrt(squaredTerms))
-
-
-
-
-
-"""2C"""
-u_array = np.linspace(0.980, 0.984, 50)
-plt.plot(u_array, abs(p(u_array) - q(u_array))/abs(p(u_array)))
-plt.show()
-
-nearTerms = xterms(u=0.983)
-squarednearTerms = np.mean(nearTerms**2)
-
-fractional_error = C * np.sqrt(squarednearTerms) / (np.sqrt(numTerms) * np.mean(nearTerms))
-
-print(fractional_error)
-
-
-
-
-
-""" 2D """
-
-def func_multiply(u):
-    return (u**8)/((u**4)*(u**4))
-
-N = 500
-u_array = u_array = np.linspace(0.98, 1.02, N)
-
-plt.plot(u_array, func_multiply(u_array)-1.)
-plt.show()
-
-
-print("Standard Deviation: ", np.std(func_multiply(u_array)-1.))
-print("Caluclated STD: ", C)
-
-
-print("""\n\nQuestion 3""")
-
-def f(x):
-    return 4 / (1+x**2)  # answer is Pi
-
-def trapz(func:callable, start, end, N):
-    """Trapezoidal integration function"""
-    h = (end-start)/N
-    s = (func(end) + func(start)) / 2
-
-    for i in range(1, N):  # dont want i == 0
-        s += func(start + i*h)
-
-    return s*h
-
-
-def simpson(func, start, end, N):
-    h = (end-start)/N
-    s = func(start) + func(end)
-
-    for i in range(1, N):
-
-        if i%2 == 0:
-            s += 2 * func(start + i*h)
-        
-        else:
-            s += 4 * func(start + i*h)
-
-
-
-    return s*h/3
-
-
-Ntrapz = 4096  # number of slices
-Nsimpson = 16  # number of slices
-a = 0
-b = 1
-
-start = time.time()
-trapzIntegral = trapz(f, a, b, Ntrapz)
-checkpoint = time.time() - start
-simpsonIntegral = simpson(f, a, b, Nsimpson)
-end = time.time() - checkpoint
-
-print("Trapezoid Rule Integral: ", trapzIntegral, "\t Error:", (np.pi - trapzIntegral), "\t Time: ", checkpoint)
-print("Simpson Rule Integral: ", simpsonIntegral, "\t Error:", (np.pi - simpsonIntegral), "\t Time: ", end)
-
-
-
-
-
-"""Practical Error Estimation"""
-print("\n")
-
-print("Error Estimation: ", (trapz(f, a, b, 32) - trapz(f, a, b, 16))/3)
-print("Error Estimation: ", (simpson(f, a, b, 32) - simpson(f, a, b, 16))/15)
-print("Error Estimation: ", (simpson(f, a, b, 64) - simpson(f, a, b, 32))/15)
